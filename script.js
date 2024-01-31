a445b68a4cff74e02aed9869d24a8c745143c3e8
caespi24 <christian.espiridion21@gmail.com>
2024-01-31 16:01:03 +0800

    updated

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ $(document).ready(function(){
         height: 800
     });
 });
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+})
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
