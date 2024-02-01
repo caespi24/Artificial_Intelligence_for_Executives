4b0b693c29d4315ae67ed63e7ca6a0141c3bb769
caespi24 <christian.espiridion21@gmail.com>
2024-02-01 14:43:44 +0800

    updated

--- script.js
+++ script.js
@@ -25,28 +25,28 @@ ScrollReveal({
 })
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-img, .dates, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .overview-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .overview-content', { origin: 'right' });
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-  sections.forEach(sec => {
+sections.forEach(sec => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
 
     if(top >= offset && top < offset + height){
-      navLinks.forEach(links => {
-        links.classList.remove('active');
-        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-      });
+        navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        });
     };
-  });
-  let header = document.querySelector('header');
+});
+let header = document.querySelector('header');
 
-  header.classList.toggle('sticky', window.scrollY > 100)
+header.classList.toggle('sticky', window.scrollY > 100)
 }
