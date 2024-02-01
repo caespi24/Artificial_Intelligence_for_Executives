09e79d165a4908fae97bf04b807f7e02aff505ac
caespi24 <christian.espiridion21@gmail.com>
2024-02-01 10:16:07 +0800

    updated

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ $(document).ready(function(){
     $('.slider').slider({
         height: 800
     });
+
+    // Collapsible init
+    $('.collapsible').collapsible();
 });
 
 ScrollReveal({
@@ -25,3 +28,25 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+  });
+  let header = document.querySelector('header');
+
+  header.classList.toggle('sticky', window.scrollY > 100)
+}
